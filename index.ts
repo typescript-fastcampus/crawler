3755409af351cd404721a5f08967facb1839d0a7
webhacking <a@hax0r.info>
2019-04-20 16:25:50 +0900

    Add rocket-punch

--- index.ts
+++ index.ts
@@ -2,7 +2,18 @@ import cron from 'node-cron';
 import {JobPlatformEndPoint, Jobs} from "./jobs";
 import {forkJoin} from "rxjs/index";
 
-cron.schedule('* * * * *', () => {
-  //
+const searchKeyword: string = 'typescript';
+const jobs$ = (new Jobs()).getJobs(JobPlatformEndPoint.RocketPunch, searchKeyword);
 
+jobs$.subscribe((job) => {
+  console.log('job', job);
+})
+
+cron.schedule('* * * * *', () => {
+  // const searchKeyword: string = 'typescript';
+  // const jobs$ = (new Jobs()).getJobs(JobPlatformEndPoint.RocketPunch, searchKeyword);
+  //
+  // jobs$.subscribe((job) => {
+  //   console.log('job', job);
+  // })
 });
--- jobs.ts
+++ jobs.ts
@@ -1,5 +1,7 @@
-import axios from 'axios';
-import {from, Observable, of} from "rxjs/index";
+import axios, {AxiosResponse} from 'axios';
+import {forkJoin, from, Observable, of} from 'rxjs/index';
+import {concatAll, concatMap, map, tap} from "rxjs/internal/operators";
+import cheerio from 'cheerio';
 
 export enum JobPlatformEndPoint {
   RocketPunch = 'https://www.rocketpunch.com',
@@ -22,28 +24,69 @@ export class Jobs {
   public getJobs(
     platformEndPoint: JobPlatformEndPoint = JobPlatformEndPoint.RocketPunch,
     searchKeyword: string
-  ): Observable<Job[]> {
+  ): Observable<any> {
     switch (platformEndPoint) {
       case JobPlatformEndPoint.RocketPunch:
-        return this.parseJobFromRocketPunch();
-      case JobPlatformEndPoint.JobKorea:
-        return this.parseJobFromJobKorea();
-      case JobPlatformEndPoint.Programmers:
-        return this.parseJobFromProgrammers();
+        return this.parseJobFromRocketPunch(searchKeyword);
+      // case JobPlatformEndPoint.JobKorea:
+      //   return this.parseJobFromJobKorea();
+      // case JobPlatformEndPoint.Programmers:
+      //   return this.parseJobFromProgrammers();
+      default:
+        return this.parseJobFromRocketPunch(searchKeyword);
     }
   }
 
-  public parseJobFromRocketPunch(): Observable<Job[]> {
+  public parseJobFromRocketPunch(keyword: string): Observable<any> {
+    return this.getPageRangeFromRocketPunch(keyword).pipe(
+      concatMap((pageRange: [number, number]) => {
+        const pages = [];
+        for (let page = pageRange[0]; page < pageRange[1]; page++) {
+          pages.push(axios.get(`${JobPlatformEndPoint.RocketPunch}/api/jobs/template`, {
+            params: {
+              page,
+              keywords: keyword
+            }
+          }))
+        }
+        return forkJoin(pages);
+      }),
+      concatAll(),
+      map((response: AxiosResponse) => cheerio.load(response.data.data.template)),
+      map(($: CheerioStatic) => $('.company.item:not(.active)').find('.company-jobs-detail > div > a.primary.link')),
+      concatAll(),
+      concatMap((detailItem: CheerioElement) => from(axios.get(`${JobPlatformEndPoint.RocketPunch}${detailItem.attribs.href}`))),
+      map((response: AxiosResponse) => cheerio.load(response.data)),
+      map(() => {
 
+      })
+    )
   }
 
-  public parseJobFromJobKorea(): Observable<Job[]> {
-
+  public getPageRangeFromRocketPunch(keyword: string): Observable<[number, number]> {
+    return from(axios.get(`${JobPlatformEndPoint.RocketPunch}/api/jobs/template`, {
+      params: {
+        keywords: keyword
+      }
+    })).pipe(
+      map((res: AxiosResponse) => cheerio.load(res.data.data.template)),
+      map(($: CheerioStatic) => {
+        return [
+          1,
+          Number($('div.tablet.computer.large.screen.widescreen.only > a:last-child').text())
+        ] as [number, number]
+      })
+    )
   }
 
-  public parseJobFromProgrammers(): Observable<Job[]> {
 
-  }
+  // public parseJobFromJobKorea(): Observable<Job[]> {
+  //
+  // }
+  //
+  // public parseJobFromProgrammers(): Observable<Job[]> {
+  //
+  // }
 
   public createJob(job: Job) {
     from(axios.post(this.endPoint))
