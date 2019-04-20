165633f97f29a5ad4a8b45bad6df26cb901b6639
webhacking <a@hax0r.info>
2019-04-20 16:49:39 +0900

    Get jobs from rocket-punch

--- jobs.ts
+++ jobs.ts
@@ -57,8 +57,15 @@ export class Jobs {
       concatAll(),
       concatMap((detailItem: CheerioElement) => from(axios.get(`${JobPlatformEndPoint.RocketPunch}${detailItem.attribs.href}`))),
       map((response: AxiosResponse) => cheerio.load(response.data)),
-      map(() => {
-
+      map(($: CheerioStatic) => {
+        return {
+          companyName: $('body > div.pusher > div.ui.vertical.center.aligned.detail.job-header.header.segment > div > div > div.job-company > div.company-name > a').text(),
+          title: $('.job-title').text(),
+          position: $('#wrap > div.four.wide.job-infoset.column > div > div:nth-child(3) > div > div:nth-child(1) > div.content').text().replace(/\n|\s|\r/g, ""),
+          annualSalary: $('#wrap > div.four.wide.job-infoset.column > div > div:nth-child(3) > div > div:nth-child(5) > div.content').text().replace(/\n|\s|\r/g, ""),
+          region: $('#wrap > div.four.wide.job-infoset.column > div > div:nth-child(3) > div > div:nth-child(2) > div.content').text().replace(/\n|\r/g, ""),
+          shortDescription: $('#job-content').text()
+        } as Job
       })
     )
   }
