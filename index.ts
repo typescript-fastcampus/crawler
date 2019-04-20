58704587351fd304dc29cd530b1552cab7cb46bd
webhacking <a@hax0r.info>
2019-04-20 17:36:24 +0900

    Missing options when pre-commit

--- index.ts
+++ index.ts
@@ -1,17 +1,20 @@
 import cron from 'node-cron';
-import {Job, JobPlatformEndPoint, Jobs} from "./jobs";
-import {forkJoin} from "rxjs/index";
-import {catchError, concatMap} from "rxjs/internal/operators";
-import {AxiosResponse} from "axios";
+import { Job, JobPlatformEndPoint, Jobs } from './jobs';
+import { forkJoin } from 'rxjs/index';
+import { catchError, concatMap } from 'rxjs/internal/operators';
+import { AxiosResponse } from 'axios';
 
 const searchKeyword: string = 'typescript';
-const jobs$ = (new Jobs()).getJobs(JobPlatformEndPoint.RocketPunch, searchKeyword);
+const jobs$ = new Jobs().getJobs(
+  JobPlatformEndPoint.RocketPunch,
+  searchKeyword
+);
 
-jobs$.pipe(
-  concatMap((job: Job) => (new Jobs()).createJob(job)),
-).subscribe((response: AxiosResponse) => {
-  console.log('Res', response.statusText);
-});
+jobs$
+  .pipe(concatMap((job: Job) => new Jobs().createJob(job)))
+  .subscribe((response: AxiosResponse) => {
+    console.log('Res', response.statusText);
+  });
 
 cron.schedule('* * * * *', () => {
   // const searchKeyword: string = 'typescript';
